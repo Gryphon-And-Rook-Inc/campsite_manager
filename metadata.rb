b2920f0b21348de3eda4ca6583d928f4b8993564
gryphonandrook <38541934+gryphonandrook@users.noreply.github.com>
2021-02-28 21:16:01 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Stephen Higgins'
 maintainer_email 'stephen@gryphonandrook.com'
 license 'All Rights Reserved'
 description 'Installs/Configures Campsite Manager'
-long_description 'Installs/Configures Campsite Manager'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 chef_version '>= 12.0'
 
